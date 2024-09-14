61a3c2d37a492bc0f8ccff6ca3be6995852cc856
Dominik Hofer <hi@dominikhofer.me>
2024-09-14 13:59:31 +0200

    Added Todo model

---
+++ app/Models/Todo.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Todo extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'due_date',
+        'completed',
+        'order',
+    ];
+
+    protected $casts = [
+        'due_date' => 'date',
+        'completed' => 'boolean',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
